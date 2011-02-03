7b07c8514ac1b68163ed024e50304feab8bf47e8
seth-schroeder <foogoof@gmail.com>
2011-02-02 21:12:10 -0500

    brand new from lein new

---
+++ src/spelunk_clj_irc/core.clj
@@ -0,0 +1,1 @@
+(ns spelunk-clj-irc.core)
---
+++ test/spelunk_clj_irc/test/core.clj
@@ -0,0 +1,6 @@
+(ns spelunk-clj-irc.test.core
+  (:use [spelunk-clj-irc.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
