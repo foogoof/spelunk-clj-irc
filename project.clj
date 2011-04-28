28dc97733d53dc1c877634e936a9f2ec1ca4c8a4
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-04-28 14:55:14 -0400

    Update dependencies to use stable releases of enlive and clj-time

--- project.clj
+++ project.clj
@@ -2,8 +2,7 @@
   :description "Some code to screenscrape and analyze the Clojure IRC logs provided by n01se.net"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [enlive "1.0.0-SNAPSHOT"]
-                 [clj-time "0.3.0-SNAPSHOT"]
-                 ]
+                 [enlive "1.0.0"]
+                 [clj-time "0.3.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
   )
